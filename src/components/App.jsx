d001be58e085f7dcfbcd97cc2e5ba4593289d9fa
isha <ishadixit642@gmail.com>
2022-06-29 10:48:55 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+import { createNotEmittedStatement } from "typescript";
+
+function App() {
+
+
+    const [notes,setNotes] = useState([]);
+    function addNote(note){
+        setNotes(prevNote => {
+            return [...prevNote,note]
+        })
+    }
+
+    function deleteNote(id){
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem,index)=>{
+                return index !== id;
+            });
+        });
+    }
+  return (
+    <div>
+      <Header />
+      <CreateArea  
+        onAdd = {addNote}
+      />
+     {notes.map((noteItem,index)=> {
+        return <Note key={index} id={index} onDelete={deleteNote} title= {noteItem.title} content={noteItem.content}/>
+     })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
